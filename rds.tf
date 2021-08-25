b0ffccb555a3f72fa54a20580bf7646151fc3552
laxmanperi <39002919+laxmanperi@users.noreply.github.com>
2021-08-25 23:13:51 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "rds" {
-  name        = length(var.name) == 0 ? "${var.project}-${var.environment}${var.tag}-rds" : var.name
+  name        = length(var.name) == 0 ? "${var.project}-${var.environment}-rds" : var.name
   description = "Our main group of subnets"
   subnet_ids  = var.subnets
 }
@@ -11,14 +11,12 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = "Admin"
+  password             = var.password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  multi_az             = var.multi_az
-  vpc_security_group_ids = [aws_security_group.sg_rds.id]
+  vpc_security_group_ids = ["sg-05b73ee9bfb8855ac"]
   db_subnet_group_name   = aws_db_subnet_group.rds.id
-  monitoring_interval    = var.monitoring_interval
   
   tags = {
     Name        =  length(var.name) == 0 ? "${var.project}-${var.environment}${var.tag}-rds" : var.name
